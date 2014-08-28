8c49e2970699b7661092826d24f383c21fa7b37d
Matt Enlow <matt@divaos.com>
2014-08-27 23:14:36 -0600

    Codebase is go

---
+++ index.js
@@ -0,0 +1,91 @@
+var _ = require('lodash');
+var request = require("request");
+var Promise = require('bluebird');
+
+function main(options) {
+    var user = options.user;
+    var repo = options.repo;
+    var releaseDate = options.releaseDate;
+    var releaseTag = options.releaseTag
+    var count = options.count || Infinity;
+
+    if (!(user && repo)) {
+        throw new Error("Must specify both github user and repo.");
+    }
+    //Looks like we're good to go. Start making promises baby!
+    var repoApiUrl = ['https://api.github.com/repos/', user, '/', repo].join('');
+
+    var releaseDatePromise = getReleaseDatePromise(releaseDate, releaseTag, repoApiUrl, user);
+    var contributorsPromise = requestPromise(repoApiUrl + '/stats/contributors', user);
+
+    return Promise.join(releaseDatePromise, contributorsPromise, count, getTopContributors);
+}
+
+function getReleaseDatePromise (releaseDate, releaseTag, repoApiUrl, user) {
+    if (releaseDate) {
+        return Promise.resolve(releaseDate);
+    }
+    // If neither releaseDate or releaseTag were specified
+    // sum all commits since the beginning of time.
+    if (!releaseTag) {
+        return resolve(0); // All time!
+    }
+
+    return requestPromise(repoApiUrl + '/releases', user).then(function (releases) {
+        var lastRelease = _.find(releases, function findLastRelease(release) {
+            return release.tag_name === releaseTag;
+        });
+
+        if (!lastRelease) {
+            return Promise.reject(releaseTag + ' not found in github releases\'s tags.');
+        }
+        //Divide by 1k to remove milliseconds to match GH datestamps
+        return lastReleaseDate = Date.parse(lastRelease.published_at) / 1000;
+    });
+}
+
+function getTopContributors(releaseDate, contributors, count) {
+    contributors =  _.map(contributors, function (contributor) {
+        var numCommitsSinceReleaseDate = _.reduce(contributor.weeks,
+            function (commits, week) {
+                if (week.w >= releaseDate) {
+                    commits += week.c;
+                }
+                return commits;
+            }, 0);
+
+        return {
+            commitCount: numCommitsSinceReleaseDate,
+            name: contributor.author.login,
+            githubUrl: contributor.author.html_url,
+            avatarUrl: contributor.author.avatar_url
+        };
+    });
+    //Get the top `count` contributors by commits
+    return _.sortBy(contributors, 'commitCount').reverse().slice(0, count);
+}
+
+function requestPromise (url, agent) {
+    return new Promise(function (resolve, reject) {
+        request({
+            url: url,
+            json: true,
+            headers: {
+                'User-Agent': agent
+            }
+        }, function (error, response, body) {
+            if (error) {
+                return reject(error);
+            }
+            if (response.statusCode >= 500) {
+                return reject('Server error on url ' + url);
+            }
+            if (response.statusCode >= 400) {
+                return reject('Client error on url ' + url);
+            }
+            return resolve(body);
+        });
+    });
+}
+
+module.exports = main;
